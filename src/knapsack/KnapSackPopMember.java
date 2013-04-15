f352db406dc772c20fdd9faf11c69967ec9f020d
John Rutherford <drummerj09@gmail.com>
2013-04-15 12:11:54 -0400

    created pop member for knapsack problem and added sorted population from ga library

---
+++ src/knapsack/KnapSackPopMember.java
@@ -0,0 +1,21 @@
+package knapsack;
+
+import model.Genome;
+import model.PopMember;
+
+/**
+ * Pop Member for the knapsack problem
+ * @author John Rutherford
+ * @version 4/15/13
+ */
+public class KnapSackPopMember extends PopMember{
+	public KnapSackPopMember(Genome genes, double score) {
+		super(genes, score);
+	}
+
+	@Override
+	public void hillClimb() {
+		// TODO Auto-generated method stub
+		
+	}
+}
